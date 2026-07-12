fef1b835967fedef8aaa0e8fc15f784c2e850a21
laharitanneru09 <laharitanneru09@gmail.com>
2026-07-12 22:49:07 +0530

    Added maxvalue.py

--- dictionaries/maxvalue.py
+++ dictionaries/maxvalue.py
@@ -1,3 +1,4 @@
+#Finding the key with max value in the dictionary
 my_dict = {"n1": 80, "n2": 45, "n3": 67, "n4": 56}
 
 min_key = list(my_dict.keys())[0]
@@ -10,3 +11,13 @@ for key in my_dict:
 
 print("Key with minimum value:", min_key)
 print("Minimum value:", min_value)
+#Time Complexity: O(n)
+#Space complexity:O(1)
+
+
+#Alternate method : using built-in function
+#min_key = min(my_dict, key=my_dict.get)
+#for this method 
+#Time Complexity: O(n)
+#Space complexity:O(1)
+
