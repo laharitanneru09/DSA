099b757a3bade8bcf77a4110ac7a749449b84022
laharitanneru09 <laharitanneru09@gmail.com>
2026-07-14 20:04:24 +0530

    Added code to find sum of elements in a list

---
+++ list/sumofelements.py
@@ -0,0 +1,13 @@
+#finding the sum of elements in a list
+lst=[37,56,9,45,2,11]
+total=0
+for i in lst:
+    total+=i
+print(total)
+#time complexity:O(n)
+#space complexity:O(1)
+
+#alternate method
+#using print(sum(lst))
+#time complexity:O(n)
+#space complexity:O(1)
