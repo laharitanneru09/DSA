d840335c2c49e97b7fbffcd400c6ebaa356690aa
laharitanneru09 <laharitanneru09@gmail.com>
2026-07-27 22:19:37 +0530

    Added code to print all keys

---
+++ dictionaries/printkeys.py
@@ -0,0 +1,9 @@
+#code to print all the keys in the dictionary
+dict1={"key1":10,"key2":20,"key3":30,"key4":40,"key5":50}
+
+
+for key in dict1:
+    print(key)
+
+#Time Complexity : O(n)
+#Space Complexity : O(1)
