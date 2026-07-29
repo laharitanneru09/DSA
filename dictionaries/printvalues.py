eb56b17822ec5ba0b6b6eba57999fd8aa6d64e22
laharitanneru09 <laharitanneru09@gmail.com>
2026-07-29 21:28:39 +0530

    Added code to print values in a dictionary

---
+++ dictionaries/printvalues.py
@@ -0,0 +1,10 @@
+# printing the values in the dictionary
+dict1={"sname":'Suzan',"ID":1021,"class":12,"section":'c',"adress":'NewJersy'}
+dict1.values()
+
+for value in dict1.values():
+    print(value)
+
+
+#Time Complexity :  O(n)
+#Space Complexity : O(1)
