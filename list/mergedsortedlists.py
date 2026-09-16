3c6d80a7bd047d4f29c9e55491d8b0878c728368
laharitanneru09 <laharitanneru09@gmail.com>
2026-09-16 22:57:30 +0530

    Added mergedsortedlists.py

---
+++ list/mergedsortedlists.py
@@ -0,0 +1,34 @@
+# Code to Merge two sorted lists without using built-in functions
+
+list1 = [1, 3, 5, 7]
+list2 = [2, 4, 6, 8]
+
+merged = []
+
+i = 0
+j = 0
+
+
+while i < len(list1) and j < len(list2):
+
+    if list1[i] <= list2[j]:
+        merged.append(list1[i])
+        i += 1
+    else:
+        merged.append(list2[j])
+        j += 1
+
+
+while i < len(list1):
+    merged.append(list1[i])
+    i += 1
+
+
+while j < len(list2):
+    merged.append(list2[j])
+    j += 1
+
+print("Merged sorted list:", merged)
+
+#   Time Complexity : O(n+m)
+#   Space Complexity : O(n+m)
